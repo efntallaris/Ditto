e272e4f0aa653f890805db3be4b58938bc33f3d6
Efstratios Ntallaris <58358951+efntallaris@users.noreply.github.com>
2025-06-27 04:02:00 +0300

    Update setup-env.sh

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -118,28 +118,6 @@ conda init zsh
 # setup-disk-part
 # use `sudo fdisk /dev/sda` to first delete all the partition
 # then carefully recreate it with a larger size at the same position
-echo "p
-d
-
-d
-
-d
-
-d
-
-d
-
-n
-p
-1
-
-
-
-No
-p
-w
-" | sudo fdisk /dev/sda
-
 # chsh
 sudo chsh $USER -s /bin/zsh
 
